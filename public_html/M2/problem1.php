792cb6fe7e6389c482dd7a8c9122ae481226a507
Rishik Danda <rishik.dandahp@gmail.com>
2023-09-25 20:21:00 -0400

    completed hw

--- public_html/M2/problem1.php
+++ public_html/M2/problem1.php
@@ -10,6 +10,11 @@ function processArray($arr)
     //note: use the $arr variable to iterate over, don't directly touch $a1-$a4
     //TODO add logic here to echo out only odd values
 
+    foreach ($arr as $num) {
+        if ($num % 2 == 1) {
+            echo $num . "<br>";
+        }
+    }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
--- public_html/M2/problem3.php
+++ public_html/M2/problem3.php
@@ -10,6 +10,19 @@ function bePositive($arr)
     echo "<br>Positive output:<br>";
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO use echo to output all of the values as positive (even if they were originally positive) and maintain the original datatype
+    foreach ($arr as $num) {
+        if (is_string($num)) {
+            if (strpos($num, '-') === 0) {
+                $posVal = substr($num, 1); // Remove the negative sign
+            } else {
+                $posVal = $num;
+            }
+            var_dump($posVal);
+        } else {
+            $posNum = abs($num);
+            var_dump($posNum);
+        }
+    }
     //hint: may want to use var_dump() or similar to show final data types
 }
 echo "Problem 3: Be Positive<br>";
